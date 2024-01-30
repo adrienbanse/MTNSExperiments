d80fddd555eb4387dbf9b4ea7f78313a8b69c89d
adrienbanse <adrien.banse@gmail.com>
2024-01-30 19:16:15 +0100

    random mdp implem

--- src/MTNSExperiments.jl
+++ src/MTNSExperiments.jl
@@ -10,9 +10,65 @@ using Distributions
 Random.seed!(1234)
 include("cantor_kantorovich.jl")
 
+function make_stochastic!(P::Matrix{Float64})
+    s, _ = size(P)
+    for i = 1:s
+        P[i, :] /= sum(P[i, :])
+    end
+end
+
+Base.@kwdef struct SimpleMDP <: MDP{Int, Symbol} 
+    state_size::Int
+    P::Dict{Symbol, Matrix{Float64}}
+    rewards::Dict{Int, Float64}
+    discount::Float64
+end
+
+POMDPs.states(mdp::SimpleMDP) = collect(1:mdp.state_size)
+POMDPs.stateindex(mdp::SimpleMDP, s::Int) = s
+struct SimpleUniform 
+    state_size::Int
+end
+Base.rand(rng::AbstractRNG, d::SimpleUniform)::Int = rand(rng, 1:d.state_size)
+POMDPs.pdf(d::SimpleUniform, s::Int) = 1 / d.state_size
+POMDPs.support(d::SimpleUniform) = (x for x ∈ 1:d.state_size)
+POMDPs.initialstate(mdp::SimpleMDP) = SimpleUniform(mdp.state_size)
+POMDPs.actions(mdp::SimpleMDP) = (:a, :b)
+Base.rand(rng::AbstractRNG, t::NTuple{L,Symbol}) where L = t[rand(rng, 1:length(t))]
+POMDPs.actionindex(mdp::SimpleMDP, a::Symbol) = a == :a ? 1 : 2
+POMDPs.isterminal(m::SimpleMDP, s::Int) = false
+POMDPs.transition(mdp::SimpleMDP, s::Int, a::Symbol) = (mdp.P)[a]
+transition_matrices(mdp::SimpleMDP) = mdp.P
+POMDPs.reward(mdp::SimpleMDP, s::Int) = get(mdp.rewards, s, 0.0)
+POMDPs.reward(mdp::SimpleMDP, s::Int, a::Symbol) = reward(mdp, s)
+POMDPs.discount(mdp::SimpleMDP) = mdp.discount
+function POMDPs.convert_a(::Type{V}, a::Symbol, m::SimpleMDP) where {V<:AbstractArray}
+    convert(V, [actionindex(m, a)])
+end
+function POMDPs.convert_a(::Type{Symbol}, vec::V, m::SimpleMDP) where {V<:AbstractArray}
+    actions(m)[convert(Int, first(vec))]
+end
+
+function generate_random_MDP(
+    state_size::Int, 
+    γ::Float64, 
+    r_range::Tuple{Float64, Float64}
+)
+    rewards = Dict{Int, Float64}() 
+    for i = 1:state_size
+        rewards[i] = rand(Distributions.Uniform(r_range...))
+    end
+    P = Dict{Symbol, Matrix{Float64}}()
+    P[:a] = rand(Distributions.Uniform(0. ,1.), state_size, state_size)
+    make_stochastic!(P[:a])
+    P[:b] = rand(Distributions.Uniform(0. ,1.), state_size, state_size)
+    make_stochastic!(P[:b])
+    return SimpleMDP(state_size, P, rewards, γ)
+end
+
 function evaluate(mdp, solver, policy)
-    rng = Random.default_rng()
-    sim = RolloutSimulator(rng=rng, max_steps = solver.max_episode_length)
+    # rng = Random.default_rng()
+    sim = RolloutSimulator(max_steps = solver.max_episode_length)
     r_tot = 0.0
     for _ ∈ 1:solver.n_eval_traj
         r_tot += simulate(sim, mdp, policy)
@@ -71,24 +127,22 @@ function q_learning_experiments(
     return mean / n_experiments
 end
 
-world_size = 20
-
-goal_target = (rand(1:world_size), rand(1:world_size))
-tprob_target = rand(Distributions.Uniform(0, 1))
-mdp_target = SimpleGridWorld(size = (world_size, world_size), rewards = Dict(GWPos(goal_target...) => 1.0), tprob = tprob_target)
+mdp_target = generate_random_MDP(10, 0.8, (3., 5.))
 
 couples = []
-n_sources = 20
+n_sources = 10
 for k = 1:n_sources
-    global couples
-    
-    goal_source = (rand(1:world_size), rand(1:world_size))
-    tprob_source = rand(Distributions.Uniform(0, 1))
-    mdp_source = SimpleGridWorld(size = (world_size, world_size), rewards = Dict(GWPos(goal_source...) => 1.0), tprob = tprob_source) 
+    global couples, t_prob
 
-    d = cantor_kantorovich(mdp_source, mdp_target; N = 4)
+    mdp_source = mdp_target = generate_random_MDP(10, 0.8, (3., 5.))
+    
+    # goal_source = (rand(1:world_size), rand(1:world_size))
+    # tprob_source = rand(Distributions.Uniform(0, 1))
+    # mdp_source = SimpleGridWorld(size = (world_size, world_size), rewards = Dict(GWPos(goal_source...) => 1.0), tprob = tprob_source) 
+
+    d = @time cantor_kantorovich(mdp_source, mdp_target; N = 5)
     println("$k / $n_sources: CK-distance computed")
-    r = q_learning_experiments(mdp_source, mdp_target)
+    r = q_learning_experiments(mdp_source, mdp_target; n_experiments = 1)
     println("$k / $n_sources: advantage computed")
     push!(couples, (d, r))
     println("$k / $n_sources: done (d = $d, Δr = $r)")
@@ -96,7 +150,7 @@ for k = 1:n_sources
 end
 
 using Plots
-p = scatter()
+p = scatter(legend = false)
 for c ∈ couples
     scatter!(p, c)
 end
--- src/cantor_kantorovich.jl
+++ src/cantor_kantorovich.jl
@@ -1,82 +1,3 @@
-function cantor_kantorovich(m1::M, m2::M; N = 3) where {T, P, M <: MDP{T, P}}
-    S = MTNSExperiments.states(m1)
-    A = actions(m1)
-    if !issetequal(S, MTNSExperiments.states(m2)) || !issetequal(A, actions(m2))
-        throw(AssertionError)
-    end
-    T1 = transition_matrices(m1)
-    T2 = transition_matrices(m2)
-
-    R1 = m1.rewards
-    R2 = m2.rewards
-
-    function ck_rec(k::Int, m::Float64, w::Vector{Int}, p1::Float64, p2::Float64, reward::Float64)
-        w_new_list = Vector{Vector{Int}}()
-        for s in S
-            w_new = push!(copy(w), stateindex(m1, s))
-            push!(w_new_list, w_new)
-        end
-        r = Vector{Float64}()
-        p1_list = Vector{Float64}()
-        p2_list = Vector{Float64}()        
-
-        a = A[1]
-
-        for s in S
-            if k == 0
-                p1_new = pdf(initialstate(m1), s)
-                p2_new = pdf(initialstate(m1), s)
-            else
-                p1_new = p1 * T1[a][w[end], stateindex(m1, s)]
-                p2_new = p2 * T2[a][w[end], stateindex(m1, s)]
-            end
-            push!(p1_list, p1_new)
-            push!(p2_list, p2_new)
-            push!(r, min(p1_new, p2_new))
-
-            rew1 = haskey(R1, s) ? R1[s] : 0
-            rew2 = haskey(R2, s) ? R2[s] : 0
-            reward += (0.5) ^ k * p1_new * p2_new * abs(rew1 - rew2)
-        end     
-
-        res = (2.0)^(-(k + 1)) * (m - sum(r))
-        if k + 1 == N
-            return res, reward
-        end
-        
-        for s ∈ S
-            idx = stateindex(m1, s)
-            if r[idx] != 0
-                inc, _ = ck_rec(k + 1, r[idx], w_new_list[idx], p1_list[idx], p2_list[idx], reward)
-                res += inc
-            end
-        end
-
-        return res, reward
-    end
-
-    # try_list = Vector{Vector{P}}()
-    # build_action_list!(try_list, N - 1, Vector{P}(), [a for a ∈ A])
-
-    # best_cand = -1
-    # best_rew = -1
-
-    # for a_list ∈ try_list
-    #     cand, rew = ck_rec(0, 1.0, Vector{Int}(), 1., 1., 0.)
-    #     if cand > best_cand
-    #         best_cand = cand
-    #         best_rew = rew
-    #     end
-    # end
-
-    res, rew = ck_rec(0, 1.0, Vector{Int}(), 1., 1., 0.)
-
-    α = 0.5
-    β = 0.5
-
-    return α * best_cand + β * best_rew
-end
-
 function build_action_list!(total::Vector{Vector{P}}, N::Int, save::Vector{P}, A::Vector{P}) where {P}
     if N == 0
         push!(total, save)
@@ -86,6 +7,59 @@ function build_action_list!(total::Vector{Vector{P}}, N::Int, save::Vector{P}, A
         save_cp = push!(copy(save), a)
         build_action_list!(total, N - 1, save_cp, A)
     end
+end
+
+function cantor_kantorovich(m1::M, m2::M; N = 3) where {T, P, M <: MDP{T, P}}
+    S = MTNSExperiments.states(m1)
+    A = actions(m1)
+    if !issetequal(S, MTNSExperiments.states(m2)) || !issetequal(A, actions(m2))
+        throw(AssertionError)
+    end
+    T1 = transition_matrices(m1)
+    T2 = transition_matrices(m2)
+
+    function ck_rec(
+        k::Int,
+        p1::Float64, 
+        p2::Float64, 
+        r::Float64,
+        w::Vector{Int}, 
+        a_list::Vector{P}
+    )
+        if k == N
+            return 2 * min(p1, p2)
+        end
+        sum = 0.
+        for s ∈ S
+            p1_new = p1 * T1[a_list[k]][w[end], stateindex(m1, s)]
+            p2_new = p2 * T2[a_list[k]][w[end], stateindex(m2, s)] # m1 or m2 shouldn't change anything here    
+            r_new = min(p1_new, p2_new)
+            if r_new != 0
+                sum += ck_rec(k + 1, p1_new, p2_new, r_new, push!(copy(w), stateindex(m1, s)), a_list)
+            end
+        end
+        return r + 0.5 * sum
+    end
+
+    a_list_list = Vector{Vector{P}}()
+    build_action_list!(a_list_list, N - 1, Vector{P}(), [a for a ∈ A])
+    
+    best_cand = 0.
+    for a_list ∈ a_list_list
+        S_rest = 0.
+        for s ∈ S
+            μ1 = pdf(initialstate(m1), s)
+            μ2 = pdf(initialstate(m2), s)  
+            r = min(μ1, μ2)
+            if r != 0
+                S_rest += ck_rec(1, μ1, μ2, r, [stateindex(m1, s)], a_list)
+            end
+        end
+        cand = 0.5 - 0.25 * S_rest
+        best_cand = max(cand, best_cand)
+    end
+    
+    return best_cand
 end
 
 
